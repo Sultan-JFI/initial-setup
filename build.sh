8b11fd6cc3f785c5f97ac9fbfa206017dae3af11
Sultan-JFI <lesson.1386@gmail.com>
2025-07-11 11:46:57 +0330

    Delete build.sh

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -o errexit
-
-# Install dependencies
-pip install -r requirements.txt
-
-# Run Django migrations
-python manage.py collectstatic --no-input
-python manage.py migrate
