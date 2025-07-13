ab40e4fb181a7cec88789eadf7c56bef149d5748
Sultan-JFI <lesson.1386@gmail.com>
2025-07-13 10:59:36 +0330

    Update urls.py

--- news_website/urls.py
+++ news_website/urls.py
@@ -3,14 +3,19 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+# ایمپورت کردن نمونه CustomAdminSite که در news/admin.py تعریف کردیم
+from news.admin import admin_site 
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('news.urls')), # <-- Include news app URLs
-    # path('accounts/', include('accounts.urls')), # Uncomment if you add accounts URLs later
+    # جایگزینی admin.site پیش‌فرض با admin_site سفارشی ما
+    path('admin/', admin_site.urls),
+    
+    path('', include('news.urls')), 
+    # path('accounts/', include('accounts.urls')),
 ]
 
 # Serve static and media files during development
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    # urlpatterns += [path("__reload__/", include("django_browser_reload.urls"))] # Uncomment if using django-browser-reload
+    # urlpatterns += [path("__reload__/", include("django_browser_reload.urls"))]
